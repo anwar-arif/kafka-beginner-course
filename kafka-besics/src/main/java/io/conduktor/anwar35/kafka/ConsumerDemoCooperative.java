7fa8019e971ddf4410a54a2954214654f8c20bea
anwar-arif <anwararif727@gmail.com>
2023-03-25 15:46:51 +0600

    kafka consumer demo

---
+++ kafka-besics/src/main/java/io/conduktor/anwar35/kafka/ConsumerDemoCooperative.java
@@ -0,0 +1,67 @@
+ package io.conduktor.anwar35.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.CooperativeStickyAssignor;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+ public class ConsumerDemoCooperative {
+     private static final Logger log = LoggerFactory.getLogger(ConsumerDemoCooperative.class.getSimpleName());
+     public static void main(String[] args) {
+         log.info("I am a kafka consumer!");
+
+         String groupId = "my-java-application";
+         String topic = "demo_java";
+
+         // create producer properties
+         Properties properties = new Properties();
+
+         // connect to conduktor playground
+         properties.setProperty("bootstrap.servers", "cluster.playground.cdkt.io:9092");
+         properties.setProperty("security.protocol", "SASL_SSL");
+         properties.setProperty("sasl.jaas.config", "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"1zep1U2Mn1Agtk9hfZwTNx\" password=\"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2F1dGguY29uZHVrdG9yLmlvIiwic291cmNlQXBwbGljYXRpb24iOiJhZG1pbiIsInVzZXJNYWlsIjpudWxsLCJwYXlsb2FkIjp7InZhbGlkRm9yVXNlcm5hbWUiOiIxemVwMVUyTW4xQWd0azloZlp3VE54Iiwib3JnYW5pemF0aW9uSWQiOjcxNzAyLCJ1c2VySWQiOjgzMTYxLCJmb3JFeHBpcmF0aW9uQ2hlY2siOiI0YjhiM2E1MS1mNDVmLTQyMDYtYTY3Yy1jYjQ5OWRkOGU1ZWUifX0.2TmXRI-zZ_7y7y2H0gi_vMGs3NK39TOA2ijEUYtKrX8\";");
+         properties.setProperty("sasl.mechanism", "PLAIN");
+
+         // create consumer configs
+         properties.setProperty("key.deserializer", StringDeserializer.class.getName());
+         properties.setProperty("value.deserializer", StringDeserializer.class.getName());
+
+         properties.setProperty("group.id", groupId);
+         properties.setProperty("auto.offset.reset", "earliest");
+         properties.setProperty("partition.assignment.strategy", CooperativeStickyAssignor.class.getName());
+
+         // create consumer
+         KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+
+         // get a reference to the main thread
+         final Thread mainThread = Thread.currentThread();
+
+         // adding the shutdown hook
+         Runtime.getRuntime().addShutdownHook(new Thread() {
+             public void run() {
+                 log.info("Detected a shutdown, let's exit by calling consumer.weakup()...");
+             }
+         });
+
+         // subscribe to a topic
+         consumer.subscribe(Arrays.asList(topic));
+
+         // poll for data
+         while (true) {
+             ConsumerRecords<String, String> records =
+                     consumer.poll(Duration.ofMillis(1000));
+             for (ConsumerRecord<String, String> record: records) {
+                 log.info("Key: " + record.key() + ", Value: " + record.value());
+                 log.info("Partition: " + record.partition() + ", Offset: " + record.offset());
+             }
+         }
+
+     }
+ }
